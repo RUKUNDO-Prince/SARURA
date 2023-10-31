f083cc2a622ce0fee1ef277ef6fcaf99b476a1cb
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-10-31 21:47:19 +0200

    created the signup page

---
+++ src/app/signup/signup.tsx
@@ -0,0 +1,7 @@
+export default function SignupPage () {
+    return (
+        <div className="flex">
+            <h1 className="text-center text-white text-2xl">Signup page</h1>
+        </div>
+    )
+}
