e7433ce5aa9d052101a3ef77686652f8181f38b4
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-11-04 07:46:14 +0200

    implementing the login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,34 @@
-export default function LoginPage () {
+"use client";
+import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+
+export default function SignupPage () {
+    const [user, setUser] = React.useState({
+        email: "",
+        password: "",
+        username: ""
+    });
+
+    const onSignup = async () => {}
+    
     return (
-        <div className="flex">
-            <h1 className="text-center text-white text-2xl ">Login page</h1>
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <div className="border border-white p-10 rounded-lg">
+                <div className="flex flex-col items-center justify-center">
+                    <h1>Log In</h1>
+                    <hr />
+                    <label htmlFor="username">Username: </label>
+                    <input className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600" id="username" type="text" value={user.username} onChange={(e) => setUser({...user, username: e.target.value})} placeholder="Enter the username" />
+                    <label htmlFor="email">Email: </label>
+                    <input className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600" id="email" type="email" value={user.email} onChange={(e) => setUser({...user, email: e.target.value})} placeholder="Enter the email" />
+                    <label htmlFor="password">Password: </label>
+                    <input className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600" id="password" type="password" value={user.password} onChange={(e) => setUser({...user, password: e.target.value})} placeholder="Enter the password" />
+                    <button className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600" onClick={onSignup}>Sign Up</button>
+                    <Link href="/login">Go to Login</Link>
+                </div>
+            </div>
         </div>
     )
 }
