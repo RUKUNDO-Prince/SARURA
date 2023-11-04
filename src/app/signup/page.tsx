f681e90bf03e551c6c96f503d4831772ebb98a2e
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-11-04 06:27:37 +0200

    implementing the signup page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,21 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+
 export default function SignupPage () {
+    const [user, setUser] = React.useState({
+        email: "",
+        password: "",
+        username: ""
+    });
+
+    const onSignup = async () => {}
+    
     return (
-        <div className="flex">
-            <h1 className="text-center text-white text-2xl">Signup page</h1>
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1>Sign Up</h1>
         </div>
     )
 }
