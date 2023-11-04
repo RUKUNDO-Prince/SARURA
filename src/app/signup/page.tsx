73b7cc0eb376fa0534f624878bd8acd67e3bb4d9
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-11-04 07:25:24 +0200

    implementing the signup page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -21,6 +21,8 @@ export default function SignupPage () {
             <input className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600" id="username" type="text" value={user.username} onChange={(e) => setUser({...user, username: e.target.value})} placeholder="Enter the username" />
             <label htmlFor="email">Email: </label>
             <input className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600" id="email" type="email" value={user.email} onChange={(e) => setUser({...user, email: e.target.value})} placeholder="Enter the email" />
+            <label htmlFor="password">Password: </label>
+            <input className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600" id="password" type="password" value={user.password} onChange={(e) => setUser({...user, password: e.target.value})} placeholder="Enter the password" />
         </div>
     )
 }
