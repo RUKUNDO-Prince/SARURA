900e6a902655617985087a6f9f6fb0ab3ec788ae
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-11-04 06:36:05 +0200

    implementing the signup page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -16,6 +16,11 @@ export default function SignupPage () {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Sign Up</h1>
+            <hr />
+            <label htmlFor="username">Username: </label>
+            <input className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600" id="username" type="text" value={user.username} onChange={(e) => setUser({...user, username: e.target.value})} placeholder="Enter the username" />
+            <label htmlFor="email">Email: </label>
+            <input className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600" id="email" type="email" value={user.email} onChange={(e) => setUser({...user, email: e.target.value})} placeholder="Enter the email" />
         </div>
     )
 }
