67b73df7eed97093b8be9a9689cdd2abb61a0f1a
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-11-09 21:51:54 +0200

    added more styles, and finished adding user

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -31,11 +31,11 @@ export default function SignupPage () {
                     <h1>Sign Up</h1>
                     <hr />
                     <label htmlFor="username">Username: </label>
-                    <input className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600" id="username" type="text" value={user.username} onChange={(e) => setUser({...user, username: e.target.value})} placeholder="Enter the username" />
+                    <input className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black" id="username" type="text" value={user.username} onChange={(e) => setUser({...user, username: e.target.value})} placeholder="Enter the username" />
                     <label htmlFor="email">Email: </label>
-                    <input className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600" id="email" type="email" value={user.email} onChange={(e) => setUser({...user, email: e.target.value})} placeholder="Enter the email" />
+                    <input className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black" id="email" type="email" value={user.email} onChange={(e) => setUser({...user, email: e.target.value})} placeholder="Enter the email" />
                     <label htmlFor="password">Password: </label>
-                    <input className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600" id="password" type="password" value={user.password} onChange={(e) => setUser({...user, password: e.target.value})} placeholder="Enter the password" />
+                    <input className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black" id="password" type="password" value={user.password} onChange={(e) => setUser({...user, password: e.target.value})} placeholder="Enter the password" />
                     <button className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600" onClick={onSignup}>{buttonDisabled ? "No Signup" : "Signup"}</button>
                     <Link href="/login">Go to Login</Link>
                 </div>
