d3a17475f1628719a72dcc5121f53c3df9fc14ab
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-11-09 20:17:12 +0200

    continue form validation

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -25,6 +25,8 @@ const userSchema = new mongoose.Schema({
     },
     forgotPasswordToken: String,
     forgotPasswordTokenExpiry: Date,
+    verifyToken: String,
+    verifyTokenExpiry: Date,
 })
 
 const User = mongoose.models.users || mongoose.model("user", userSchema);
