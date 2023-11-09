1a5ef5c53c883183d65c1010832884ea5588a635
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-11-09 20:08:34 +0200

    continue form validation

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -6,6 +6,11 @@ const userSchema = new mongoose.Schema({
         required: [true, "Please provide a username"],
         unique: true,
     },
+    email: {
+        type: String,
+        required: [true, "Please provide an email"],
+        unique: true,
+    },
 })
 
 const User = mongoose.models.users || mongoose.model("user", userSchema);
