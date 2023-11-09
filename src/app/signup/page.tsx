e10aeb54385a65e28ad54c52577dd0de932a2187
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-11-09 21:46:54 +0200

    added a dynamic button

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -36,7 +36,7 @@ export default function SignupPage () {
                     <input className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600" id="email" type="email" value={user.email} onChange={(e) => setUser({...user, email: e.target.value})} placeholder="Enter the email" />
                     <label htmlFor="password">Password: </label>
                     <input className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600" id="password" type="password" value={user.password} onChange={(e) => setUser({...user, password: e.target.value})} placeholder="Enter the password" />
-                    <button className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600" onClick={onSignup}>Sign Up</button>
+                    <button className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600" onClick={onSignup}>{buttonDisabled ? "No Signup" : "Signup"}</button>
                     <Link href="/login">Go to Login</Link>
                 </div>
             </div>
