cb9093499f6239c7a1ec9e869d3b913361783222
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-11-09 21:02:07 +0200

    creating a new user

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,27 @@
+import { connect } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel"
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+
+connect()
+
+export async function POST (request: NextRequest) {
+    try {
+        const reqBody = await request.json()
+        const { username, email, password } = reqBody
+
+        console.log(reqBody)
+
+        // CHECK IF THE USER ALREADY EXISTS
+        const user = await User.findOne({email})
+        if (user) {
+            return NextResponse.json({error: "User already exists"}, {status: 400})
+        }
+
+        // HASH PASSWORD
+        const salt = await bcryptjs.genSalt(10)
+        const hashedPassword = await bcryptjs.hash(password, salt)
+    } catch (error: any) {
+        return NextResponse.json({error: error.message}, {status: 500})
+    }
+}
