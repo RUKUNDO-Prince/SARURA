f01c86c0e7930ce9cf5da29502d3aa833e68de24
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-11-18 03:17:28 +0200

    implemented the signup functionality

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,6 +3,7 @@ import React, { useEffect } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import axios from "axios";
+import toast from "react-hot-toast";
 
 export default function SignupPage () {
     const router = useRouter();
@@ -17,9 +18,17 @@ export default function SignupPage () {
 
     const onSignup = async () => {
         try {
+            setLoading(true);
+            const response = await axios.post("/api/users/signup", user);
+            console.log("Signup success", response.data);
+            router.push("/login");
             
-        } catch (error) {
+        } catch (error: any) {
+            console.log("Signup failed", error.message);
             
+            toast.error(error.message);
+        } finally {
+            setLoading(false);
         }
     }
 
